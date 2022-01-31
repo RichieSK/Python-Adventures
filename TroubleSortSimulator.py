16cfeaa3a825afb1f1ef9550e1d59951180b824d
RichieSK <81282315+RichieSK@users.noreply.github.com>
2022-01-31 22:31:04 +0530

    Create TroubleSortSimulator.py

---
+++ TroubleSortSimulator.py
@@ -0,0 +1,32 @@
+def simulate_trouble_sort(lst):
+    #Trouble sort is basically splitting an array into two halves(odd indices
+    # and even indices).
+    #So, we'll sort those two arrays and then compare adjacent elements.
+    
+    even = sorted(lst[::2])
+    odd = sorted(lst[1::2])
+    faulty_index = None
+    for index in range(len(odd)):
+        #First we'll compare if the corresponding odd index(say 1) contains a
+        # lesser value than the corresponding even index(say 0).
+        #This compares indices (0,1) (2,3) and so on.
+        #Since the number of elements in the odd array 
+        if odd[index] < even[index]:
+            faulty_index = index*2
+            break
+        #We also have to compare indices (1,2), (3,4) and so on.
+        #So we compare ith index of odd with i+1 index of even.
+        #There is an edge case where the number of elements in both arrays are
+        # equal, where we might go out of bounds in array odd.
+        elif index < len(even)-1 and odd[index] > even[index+1]:
+            faulty_index = index*2+1
+            break
+    if faulty_index is None:
+        print("The array will be sorted")
+    else:
+        print("The array won't be sorted properly and index {} will be faulty.".format(faulty_index)) 
+
+simulate_trouble_sort([2, 1])
+simulate_trouble_sort([1, 3, 2])
+simulate_trouble_sort([0, 1, 5, 3])
+simulate_trouble_sort([1, 7, 6, 2])
