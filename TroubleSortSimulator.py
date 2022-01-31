629acbc7615486f97f1f7ef2e7cb303281e711ac
RichieSK <81282315+RichieSK@users.noreply.github.com>
2022-01-31 22:56:15 +0530

    Combined arrays instead of tedious comparison
    Although the comparison between the indices of the two sorted sub-arrays might be slightly more efficient, I combined the arrays and tested whether the result is sorted for better readability.

--- TroubleSortSimulator.py
+++ TroubleSortSimulator.py
@@ -5,26 +5,37 @@ def simulate_trouble_sort(lst):
     
     even = sorted(lst[::2])
     odd = sorted(lst[1::2])
-    faulty_index = None
-    for index in range(len(odd)):
-        #First we'll compare if the corresponding odd index(say 1) contains a
-        # lesser value than the corresponding even index(say 0).
-        #This compares indices (0,1) (2,3) and so on.
-        #Since the number of elements in the odd array 
-        if odd[index] < even[index]:
-            faulty_index = index*2
+    combined_array = []
+    for index in range(len(even)):
+        combined_array.append(even[index])
+        if index < len(odd):
+            combined_array.append(odd[index])
+    for index in range(len(combined_array)-1):
+        if combined_array[index] > combined_array[index+1]:
+            print("The array won't be sorted properly and index {} will be faulty.".format(index)) 
             break
-        #We also have to compare indices (1,2), (3,4) and so on.
-        #So we compare ith index of odd with i+1 index of even.
-        #There is an edge case where the number of elements in both arrays are
-        # equal, where we might go out of bounds in array odd.
-        elif index < len(even)-1 and odd[index] > even[index+1]:
-            faulty_index = index*2+1
-            break
-    if faulty_index is None:
-        print("The array will be sorted")
     else:
-        print("The array won't be sorted properly and index {} will be faulty.".format(faulty_index)) 
+        print("The array will be sorted")
+#     faulty_index = None
+#     for index in range(len(odd)):
+#         #First we'll compare if the corresponding odd index(say 1) contains a
+#         # lesser value than the corresponding even index(say 0).
+#         #This compares indices (0,1) (2,3) and so on.
+#         #Since the number of elements in the odd array 
+#         if odd[index] < even[index]:
+#             faulty_index = index*2
+#             break
+#         #We also have to compare indices (1,2), (3,4) and so on.
+#         #So we compare ith index of odd with i+1 index of even.
+#         #There is an edge case where the number of elements in both arrays are
+#         # equal, where we might go out of bounds in array odd.
+#         elif index < len(even)-1 and odd[index] > even[index+1]:
+#             faulty_index = index*2+1
+#             break
+#     if faulty_index is None:
+#         print("The array will be sorted")
+#     else:
+#         print("The array won't be sorted properly and index {} will be faulty.".format(faulty_index)) 
 
 simulate_trouble_sort([2, 1])
 simulate_trouble_sort([1, 3, 2])
